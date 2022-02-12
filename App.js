091b83afedb449b72f17aad7c3a2398a11353ed3
ngreene9 <ngreene9@gatech.edu>
2022-02-12 13:31:16 -0500

    Minor Updates
    - Figured out how to customize Marker popups (using Callout)
    - Created ReadMe file for Version 0.1.0

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Image, Modal, Alert, SafeAreaView, ImageBackground, StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker, Callout} from 'react-native-maps';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const image = { uri: 'https://www.georgiaencyclopedia.org/wp-content/uploads/2021/02/ivan-allen-jr_002-740x1080.jpg' };
 const Stack = createNativeStackNavigator();
 
 function HomeScreen({navigation} ) {
@@ -48,7 +47,25 @@ function MapScreen() {
       longitudeDelta: 0.04,
       }}
       >
-        <Marker coordinate={{latitude: 33.739, longitude: -84.389}} title='Fulton County Stadium' />
+        <Marker
+        coordinate={{latitude: 33.739, longitude: -84.389}} 
+        title={'Fulton County Stadium'}
+        description={'Original home of both the Atlanta Braves and Atlanta Falcons'}
+        >
+          <Callout tooltip>
+            <View>
+              <View style={mapStyles.bubble}>
+                  <Text style={mapStyles.name}>Fulton County Stadium</Text>
+                  <Image 
+                    style={mapStyles.image}
+                    source={require('./images/fultonCounty.jpg')}
+                   />
+               </View>
+               <View style = {mapStyles.arrowBorder}/>
+               <View style ={mapStyles.arrow}/>
+            </View>
+          </Callout>
+        </Marker>
       </MapView>
     </View>
   );
@@ -81,8 +98,42 @@ const mapStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
+  bubble: {
+    flexDirection: 'column',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding:15,
+    width: 150,
+  },
+  arrow: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#fff',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -32,
+  },
+  arrowBorder: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#007a87',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -0.5,
+  },
+  name: {
+        fontSize: 16,
+        marginBottom: 5,
+  },
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height, 
+  },
+  image: {
+        width: 120,
+        height: 80,
   },
 });
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# **Mapping Ivan Allen's Atlanta**
+Atlanta is one of the fastest growing cities in the United States, but the rapid development has come at the cost of many of Atlanta's most storied civil rights landmarks. Our project intends to create an easy-to-navigate digital archive of civil rights era Atlanta. Users can view historic landmarks via our curated tour, or simply explore the archive on their own.
+# Release Notes
+
+## Version 0.1.0
+
+### New Features
+* Implemented React-Native Callouts into map markers
+* Added Images into marker popups to preview site
+* Prepared to implement navigation from marker popup to information screen
+
+### Bug Fixes
+* Fixed issue where Marker components could sometimes be inappropriately sized
+
+### Known Issues
+* **None as of now**![Happy](https://ih1.redbubble.net/image.1759229733.7812/flat,128x,075,f-pad,128x128,f8f8f8.jpg)
+>_but expecting this to change soon_
+![Sadge](https://ih1.redbubble.net/image.1249935413.9251/flat,128x128,075,t-pad,128x128,f8f8f8.jpg)
